b47e884a338142fd4b1c63b1cfe8f7bddb954c28
sjb8ifdmvm <cs226500aa@gmail.com>
2022-03-21 20:05:11 +0800

    修改enum 為 EnumClass

--- =Include/WICTextureLoader.h
+++ =Include/WICTextureLoader.h
@@ -37,7 +37,7 @@
 
 namespace DirectX
 {
-    enum WIC_LOADER_FLAGS : uint32_t
+    enum class WIC_LOADER_FLAGS : uint32_t
     {
         WIC_LOADER_DEFAULT      = 0,
         WIC_LOADER_FORCE_SRGB   = 0x1,
