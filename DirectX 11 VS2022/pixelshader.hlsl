14de8588115575d3976b597ed23708e704f4c62f
cs226500aa <cs226500aa@gmail.com>
2022-08-07 20:10:08 +0800

    fix warning

--- DirectX 11 VS2022/pixelshader.hlsl
+++ DirectX 11 VS2022/pixelshader.hlsl
@@ -1,14 +1,19 @@
+cbuffer alphaBuffer : register(b0)
+{
+    float alpha;
+}
+
 struct PS_INPUT
 {
     float4 inPosition : SV_POSITION;
     float2 inTexCoord : TEXCOORD;
 };
 
-Texture2D objTexture : TEXTURE : register(t0);
+Texture2D<float> objTexture : TEXTURE : register(t0);
 SamplerState objSamplerState : SAMPLER : register(s0);//範例 (可連續製作多個)
 
 float4 main(PS_INPUT input) : SV_TARGET
 {
     float3 pixelColor = objTexture.Sample(objSamplerState, input.inTexCoord);
-    return float4(pixelColor, 1.0f);
+    return float4(pixelColor, alpha);
 }
